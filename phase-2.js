ec31f4b8923389f5fa1c3e217707e303bca85463
Ruben Cuello <kuelmusic@gmail.com>
2024-05-07 19:58:25 -0400

    02

--- phase-2.js
+++ phase-2.js
@@ -2,13 +2,25 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
+  let res = []
+ for (let i = 1; i <= 10; i++) {
+  res.push(addNums(i * increment))
 
+ }
+ return res
 }
 
 // Runs `addManyNums` in 10 increasing increments
-function addManyNums10(increment) {
-  // Fill this in
+function addManyNums10(increment, count = 1, res = []) {
+
+  if(count > 10) {
+    return res
+  }
+
+   res.push(addManyNums(count * increment))
+
+
+ return addManyNums10(increment, count + 1,  res)
 
 }
 
