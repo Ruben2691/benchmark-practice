e5f5969cc7bf82c2cf52f1d565cad8e52a945490
Imani Roberts <imaniroberts.dev@gmail.com>
2024-05-07 19:20:44 -0400

    finished

--- phase-1.js
+++ phase-1.js
@@ -1,16 +1,19 @@
 // Adds up positive integers from 1-n
 function addNums(n) {
-  // Fill this in
-
+  let sum = 0;
+  for (let index = 0; index <= n; index++) {
+    sum += index;
+  }
+  return sum;
 }
-
 
 // Adds up values of addNums(1) through addNums(n)
 function addManyNums(n) {
-  // Fill this in
-
+  let sum = 0;
+  for (let index = 0; index <= n; index++) {
+    sum += addNums(index);
+  }
+  return sum;
 }
-
-
 
 module.exports = [addNums, addManyNums];
